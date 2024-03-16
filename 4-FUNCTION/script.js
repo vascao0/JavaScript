49dfdaacf8f953317def724df41bfe3eb2cb3b0a
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2024-03-16 13:07:16 -0300

    functions

---
+++ 4-FUNCTION/script.js
@@ -0,0 +1,21 @@
+function ImprimirNoConsole () {
+    console.log("VASCO")
+}
+
+ImprimirNoConsole()
+
+function ImprimirUmNúmero (num) {
+    if (num == 2) {
+        console.log("é 2")
+    } else {
+        console.log("é oto number")
+    }
+}
+
+function NumeroAleatorio () {
+    console.log(Math.random() * 0.9)
+}
+
+NumeroAleatorio()
+
+ImprimirUmNúmero(2)
