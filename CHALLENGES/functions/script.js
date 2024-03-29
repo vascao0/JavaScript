4a505e3a3634401f9cfacf41cd267f1316ed6795
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2024-03-28 22:25:20 -0300

    functios past

---
+++ CHALLENGES/functions/script.js
@@ -0,0 +1,30 @@
+// EX 01
+
+function print() {
+    console.log("Hello World")
+}
+
+print()
+
+
+// EX 02
+function ex02 (idade) {
+    console.log(`Você tem ${idade} anos`)
+}
+
+ex02(18)
+
+// EX 03
+function ex03 (a,b) {
+    console.log(a+b)
+    return a + b
+}
+
+ex03(6,8)
+
+// EX04
+function ex04 (a) {
+    console.log(Math.random() *a) + 1
+}
+
+ex04(5)
