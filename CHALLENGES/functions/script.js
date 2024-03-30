e3987042e54ab20b98ca9c68c5cd9447ccbb3786
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2024-03-30 14:32:58 -0300

    Update script.js

--- CHALLENGES/functions/script.js
+++ CHALLENGES/functions/script.js
@@ -59,3 +59,21 @@ function ex07 (negative) {
 }
 
 ex07(8)
+
+// EX08
+function ex08 (string) {
+    if (string.length == 10){
+        console.log("Ta longo")
+    } else{
+        console.log("Texto dentro do limite")
+    }
+}
+
+ex08("vvvvvvvvvv")
+
+// EX09
+function ex09 (n1,n2) {
+    console.log((n1) ** n2)
+}
+
+ex09(2,3)
