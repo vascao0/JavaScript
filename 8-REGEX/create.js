9fe959ec622fc0f28494cb9efb19297073a772fd
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2024-10-23 21:19:26 -0300

    regex

---
+++ 8-REGEX/create.js
@@ -0,0 +1,5 @@
+let rgx1 = new RegExp(/a/ig)
+let rgx2 = /a/ig
+
+//i case-insensitive
+//g global-case
---
+++ 8-REGEX/joint.js
@@ -0,0 +1,5 @@
+// [] = CONJUNTO
+console.log(/[a-z]/ig.test("oi"))
+console.log(/\d/ig.exec('10'))
+
+console.log(/a.b/ig.exec('aaabcd'))
